0cab1f35e164d9f0df5ba850da23d990409d86ba
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-05-11 09:07:20 -0700

    0512 new mqtt_client.py

--- hw3/mqtt_client.py
+++ hw3/mqtt_client.py
@@ -3,6 +3,9 @@ import time
 
 import serial
 import time
+
+
+
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev, 9600)
 
@@ -16,14 +19,32 @@ mqttc = paho.Client()
 host = "172.20.10.2"
 topic1 = "Angle selection"
 topic2 = "Angle detection"
+count = 0
+flag = 0
+
 
 # Callbacks
 def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
-    print("Message: " + str(msg.payload) + "\n")
-    s.write(bytes("/Leave_Mode/run\r\n", 'UTF-8'))
+    global flag, count
+    print("flag: ", flag)
+    num = str(msg.payload)
+    if flag == 0:
+        print("Message: " + str(msg.payload) + "\n")
+        s.write(bytes("/Leave_Mode/run\r\n", 'UTF-8'))
+        flag = 1
+    else:
+        if count != 9:
+            count+=1
+            print("count: ", count)
+            print("Message: " + str(msg.payload) + "\n")
+        else:
+            print("Message: " + str(msg.payload) + "\n")
+            s.write(bytes("/Leave_Mode/run\r\n", 'UTF-8'))
+            count = 0
+            flag = 0
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
